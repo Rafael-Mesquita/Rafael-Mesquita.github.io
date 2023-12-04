f60738d23a900a1bfc27d9977cc356f9af3fa02d
Rafael Mesquita <rafael_senac@hotmail.com>
2023-12-04 18:17:56 -0300

    first commit

---
+++ scripts.js
@@ -0,0 +1,7 @@
+function submitForm() {
+    const name = document.getElementById("name").value;
+    const email = document.getElementById("email").value;
+    const message = document.getElementById("message").value;
+  
+    alert(`Name: ${name} \nEmail: ${email} \nMessage: ${message}`);
+  }
